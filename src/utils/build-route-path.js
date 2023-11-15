ac65a7b89c84c41c41ad3bf26202385f528da929
SavioLuizz <savioluiz546@gmail.com>
2023-11-15 16:50:51 -0300

    creat routes witch paramters / RegEx

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,6 +1,10 @@
 
 export function buildRoutePath(path){
     const routeParemetersRegex = /:([a-zA-z]+)/g
+    const pathWithParams = path.replaceAll(routeParemetersRegex, '(?<$1>[a-z0-9\-_]+)')
+    //console.log(pathWithParams)
 
-    console.log(Array.from(path.matchAll((routeParemetersRegex))))
+    const pathRegex = new RegExp(`^${pathWithParams}`)
+
+    return pathRegex;
 }
